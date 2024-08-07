b27982befad6a25d6fa4597d347bb21dd757dd75
UnBaguette <149879995+UnBaguette@users.noreply.github.com>
2024-08-07 10:01:55 +0700

    clean up :3

--- MODEL/EVALUATE.py
+++
@@ -1,5 +0,0 @@
-
-def evaluate(X_test, y_test_ohe, model):
-    loss, accuracy = model.evaluate(X_test, y_test_ohe)
-
-    return loss, accuracy
--- MODEL/INFERENCE.py
+++
@@ -1,4 +0,0 @@
-def inference(model, X_new, y):
-    y_new = model.predict(X_new)
-    y_new_label = y_new.argmax(axis=1)
-    y[y_new_label]
